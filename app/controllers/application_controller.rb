b4db8677bef73a29c349f0f327c47445cee500d3
benjamin <benjamin.olmsted@gmail.com>
2021-09-24 09:29:30 -0700

    adds full user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
+
+  private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
 
 end
